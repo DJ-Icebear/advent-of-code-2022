3ec6e69969cf9276c2b25e153a5122665931bfab
Björn <bjornhelgeson@gmail.com>
2022-12-10 19:50:38 +0100

    Refactor and add solution for input file

--- day-3/day-3-task-1.test.ts
+++ day-3/day-3-task-1.test.ts
@@ -10,24 +10,20 @@ describe('Task 1', () => {
 
   it('returns correct score for example input', async () => {
     const input = await readFile('day-3/example.txt', 'binary')
-
     const rucksacks = input.split('\n')
 
-    const sumTotal = rucksacks.reduce((sum, rucksack) => {
-      const lowerCompartment = rucksack.slice(0, rucksack.length / 2)
-      const upperCompartment = rucksack.slice(rucksack.length / 2, rucksack.length)
-
-      const intersection = Array.from(lowerCompartment).filter((letter) =>
-        Array.from(upperCompartment).includes(letter)
-      )
-
-      const sharedLetter = intersection[0]
-      const value = getPrioValue(sharedLetter)
-
-      return sum + value
-    }, 0)
+    const sumTotal = getRucksackSum(rucksacks)
 
     expect(sumTotal).toBe(157)
+  })
+
+  it('returns correct score for example input', async () => {
+    const input = await readFile('day-3/input.txt', 'binary')
+    const rucksacks = input.trim().split('\n')
+
+    const sumTotal = getRucksackSum(rucksacks)
+
+    expect(sumTotal).toBe(7737)
   })
 })
 
@@ -35,4 +31,18 @@ const getPrioValue = (sharedLetter: string): number => {
   const isLowerCase = sharedLetter.toLowerCase() === sharedLetter
   if (isLowerCase) return sharedLetter.charCodeAt(0) - 96
   return sharedLetter.charCodeAt(0) - 38
+}
+
+const getRucksackSum = (rucksacks: string[]) => {
+  return rucksacks.reduce((sum, rucksack) => {
+    const lowerCompartment = rucksack.slice(0, rucksack.length / 2)
+    const upperCompartment = rucksack.slice(rucksack.length / 2, rucksack.length)
+
+    const intersection = Array.from(lowerCompartment).filter((letter) => Array.from(upperCompartment).includes(letter))
+
+    const sharedLetter = intersection[0]
+    const value = getPrioValue(sharedLetter)
+
+    return sum + value
+  }, 0)
 }
