0e47ca91aa6d4526ea62f9ce9af6a9228be52d7f
Björn <bjornhelgeson@gmail.com>
2022-12-12 08:59:30 +0100

    Add solution for task 2

--- day-5/day-5-task-1.test.ts
+++ day-5/day-5-task-1.test.ts
@@ -1,7 +1,7 @@
 import { readFile } from 'fs/promises'
 
 describe('Task 1', () => {
-  it('returns correct score for example input', async () => {
+  it('returns correct string for example input', async () => {
     const input = await readFile('day-5/example.txt', 'binary')
     const [crateState, instructions] = input.split('\n\n')
 
@@ -9,6 +9,38 @@ describe('Task 1', () => {
     const response = getResponse(crateArray, instructions)
 
     expect(response).toBe('CMZ')
+  })
+
+  it('returns correct string for input file', async () => {
+    const input = await readFile('day-5/input.txt', 'binary')
+    const [crateState, instructions] = input.split('\n\n')
+
+    const crateArray = getCrateArray(crateState)
+    const response = getResponse(crateArray, instructions)
+
+    expect(response).toBe('WHTLRMZRC')
+  })
+})
+
+describe('Task 2', () => {
+  it('returns correct string for example input', async () => {
+    const input = await readFile('day-5/example.txt', 'binary')
+    const [crateState, instructions] = input.split('\n\n')
+
+    const crateArray = getCrateArray(crateState)
+    const response = getResponse(crateArray, instructions)
+
+    expect(response).toBe('MCD')
+  })
+
+  it('returns correct string for example input', async () => {
+    const input = await readFile('day-5/input.txt', 'binary')
+    const [crateState, instructions] = input.trim().split('\n\n')
+
+    const crateArray = getCrateArray(crateState)
+    const response = getResponse(crateArray, instructions)
+
+    expect(response).toBe('MCD')
   })
 })
 
@@ -52,14 +84,15 @@ const getResponse = (crateArray: CrateArray, instructions: string): string => {
 const getTopCrates = (crateArray: CrateArray): string => crateArray.reduce((acc, stack) => acc + stack.pop(), '')
 
 const getCrateArray = (crateString: string): CrateArray => {
-  const numberOfStacks = 3
-  const stacks: CrateArray = [[], [], []]
-
   const rows = crateString.split('\n').reverse()
+  const numbersRow = rows.shift() as string
+
+  const numberOfStacks = parseInt(numbersRow[numbersRow.length - 2] as string)
+  const stacks: CrateArray = new Array(numberOfStacks).fill(0).map((_) => [])
+
   const rowLength = rows[0].length
 
-  const allButFirstRow = rows.slice(1)
-  allButFirstRow.forEach((row) => {
+  rows.forEach((row) => {
     for (
       let letterPosition = 1, currentStack = 0;
       letterPosition < rowLength;
@@ -74,12 +107,19 @@ const getCrateArray = (crateString: string): CrateArray => {
 
 const moveCrates = (crateArray: CrateArray, instruction: string): void => {
   const array = instruction.split(' ')
-  const [numberOfCrates, fromCrate, toCrate] = array
+  const [numberOfCrates, fromCrateNumber, toCrateNumber] = array
     .filter((word) => !isNaN(parseInt(word)))
     .map((item) => parseInt(item))
 
-  for (let index = 0; index < numberOfCrates; index++) {
-    const popped = crateArray[fromCrate - 1].pop()
-    popped && crateArray[toCrate - 1].push(popped)
-  }
+  console.log({ numberOfCrates, fromCrateNumber, toCrateNumber })
+  const fromCrate = crateArray[fromCrateNumber - 1]
+  console.log({ fromCrate })
+  const crateToMove = fromCrate.splice(-numberOfCrates)
+  console.log({ crateToMove })
+  crateArray[toCrateNumber - 1] = crateArray[toCrateNumber - 1].concat(crateToMove)
+  console.log({ crateArray })
+  // for (let index = 0; index < numberOfCrates; index++) {
+  //   const popped = crateArray[fromCrate - 1].pop()
+  //   popped && crateArray[toCrate - 1].push(popped)
+  // }
 }
